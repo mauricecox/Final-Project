7fed5ee6e618729eda1c72c698707e806df6293c
Modesto Cabrera <cabreramod@gmail.com>
2015-12-14 17:14:47 -0500

    Update and rename README.md to README.rs

---
+++ README.rs
@@ -0,0 +1,86 @@
+============
+Title
+============
+
+Persons
+=========
+
+Overview
+--------------
+
+Overview
+
+Process
+---------------------------
+
+Process:
+
+Issues:
+==================
+
+Isse1
+-----
+
+Problem Scenario: 
+
+Current Alternative:
+
+Value Proposition: 
+
+Isse2
+---------------------------
+
+Problem Scenario:
+
+Current Alternative:
+
+Value Proposition: 
+
+
+Conclusions
+=============
+
+
+ReCap
+-----------------------------
+
+Recap over project:
+
+Overview
+-----------------------
+
+Conclusion Overview and ending:
+
+oV
+^^^^^^^^^^^^^^^^^^^
+
+[Scenario 1]
+````````````
+
+::
+
+    Problem1
+        problem1
+	    Problem1
+        ...
+    Conclusion1
+    Conclusion1
+        Conclusion1
+	Conclusion1
+	  Conclusion1
+        …
+
+[Scenario 2]
+````````````
+
+::
+
+    Problem2
+        Problem2
+          Problem2
+              Problem2
+              	Problem2
+        ...
+    Conclusion2
+    Conclusion2
+        …
